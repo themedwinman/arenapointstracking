6baa72368125deb33238898dc0cca22eb5771f64
Oli Medwin <olivermedwin585@gmail.com>
2024-06-05 15:08:15 +1200

    added proper header and continued working on 0auth with google profile picture for avatar

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -8,7 +8,6 @@ const Login = () => {
         return <>
             Signed in as {session?.user?.email} <br />
             <p>Welcome {session?.user?.name}</p>
-            <Avatar src={session?.user?.image ?? undefined} alt={session?.user?.name ?? undefined} />
             <button onClick={() => signOut()}>Sign out</button>
         </>
     }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Component } from "react";
 import type { AppProps } from "next/app";
 
 export default function App({

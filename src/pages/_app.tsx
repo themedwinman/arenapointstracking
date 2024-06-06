091bfb4588cc620b6258c7ca9f33a637edcaaf35
themedwinman <129802052+themedwinman@users.noreply.github.com>
2024-06-06 14:54:02 +1200

    fixing routing and typing issues and getting rid of a rogue colon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,18 @@ import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Component } from "react";
 import type { AppProps } from "next/app";
+import { Session } from 'next-auth';
+
+interface CustomPageProps {
+  session: Session;
+}
 
 export default function App({
   Component, pageProps: { session, ...pageProps} 
-}){ 
+}: AppProps<CustomPageProps>){ 
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,9 +3,6 @@ import React from "react";
 
 const dashboard = () => {
   return <h1>Dashboard</h1>;
-  
-  
-
 };
 
 export default dashboard;

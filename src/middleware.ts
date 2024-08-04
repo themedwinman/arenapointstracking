8b727b12d3871b131305dac53d0bd22632d0a9dd
Oli Medwin <oli.medwin@kingsway.school.nz>
2024-08-04 14:58:23 +1200

    quick relocation of middleware.ts and changin a couple things fixes the issue and the app requires the user to be authenitcated to view any page.

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/auth/signin", //tells the app to show the sign-in page when the user is not authenticated
+    signOut: "/auth/signout", //ditto for sign-out
+    error: "/auth/error", // Error code passed in query string as ?error=
+  },
+});

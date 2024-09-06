58a264b94ee4d85bd03de04abfd71cf4513e42a0
Oli Medwin <oli.medwin@kingsway.school.nz>
2024-09-06 23:38:11 +1200

    fixed missing key prop in dataribbon file

--- src/components/Dashboard/DataRibbon/DataRibbon.tsx
+++ src/components/Dashboard/DataRibbon/DataRibbon.tsx
@@ -6,23 +6,20 @@ import scss from "./DataRibbon.module.scss";
 import { TotalPointsArray } from "@/components/mockData";
 import { houses } from "@/helper/Util";
 
-
-
 const DataRibbon = () => {
-    return (
-<Grid container gap={2} className={scss.dataRibbon} sx={{"--columns": `repeat(clamp(1, ${houses.length},6), minmax(0, 1fr))`}}>
-  {houses.map((house, index) => (<Grid>
-        <DataCard
-          title={`Total ${house} Points`}
-          value= {String(TotalPointsArray[index])}
-          description={
-            `Total amount of ${house} House Points this year`
-          }
-        />
-      </Grid>))}
+  return (
+    <Grid container gap={2} className={scss.dataRibbon} sx={{ "--columns": `repeat(clamp(1, ${houses.length},6), minmax(0, 1fr))` }}>
+      {houses.map((house, index) => (
+        <Grid key={index}>
+          <DataCard
+            title={`Total ${house} Points`}
+            value={String(TotalPointsArray[index])}
+            description={`Total amount of ${house} House Points this year`}
+          />
+        </Grid>
+      ))}
     </Grid>
-
-
-    )};
+  );
+};
 
 export default DataRibbon;
